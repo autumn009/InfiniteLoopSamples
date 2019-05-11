95d37750d75e725fd67bf4e4149089665b412845
Akira Kawamata <autumn@piedey.co.jp>
2019-05-11 20:04:44 +0900

    add Wait

---
+++ Wait/Wait/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Wait
+{
+    class Program
+    {
+        static void Main()
+        {
+            var evt = new AutoResetEvent(false);
+            Console.WriteLine("Start to wait");
+            evt.WaitOne();
+            Console.WriteLine("Done to wait");
+        }
+    }
+}
